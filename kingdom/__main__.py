bd60b91cada47125b31b54dfe41b5f6c0b20119e
Xcilik <149920327+Xcilik@users.noreply.github.com>
2024-06-08 01:55:02 +0700

    Update __main__.py

--- kingdom/__main__.py
+++ kingdom/__main__.py
@@ -2,10 +2,22 @@ import asyncio
 import sys
 from os import execl
 from pyrogram import idle
+from importlib import import_module
 
 from kingdom import *
+from kingdom.modules import loadModule
+
 
 LOOP = asyncio.get_event_loop()
+
+
+async def loadPlugins():
+    modules = loadModule()
+    for mod in modules:
+        imported_module = import_module(f"kingdom.modules.{mod}")
+
+    console.info("Plugins installed")
+
 
 async def main():
     await bot.start()
