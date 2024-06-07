179a3c8bcc4838cdfaf4c843e1d70f68e391868c
Xcilik <149920327+Xcilik@users.noreply.github.com>
2024-06-08 01:16:13 +0700

    Create handler.py

---
+++ kingdom/core/handler.py
@@ -0,0 +1,44 @@
+from pyrogram import filters
+from config import ADMINS
+from kingdom import bot
+
+
+class FILTERS:
+    ME = filters.me
+    GROUP = filters.group
+    PRIVATE = filters.private
+    OWNER = filters.user(ADMINS)
+    ME_PRIVATE = filters.me & filters.private
+    ME_GROUP = filters.me & filters.group
+    ME_OWNER = filters.me & filters.user(ADMINS)
+
+class KINGDOM:
+    def BOT(command, filter=FILTERS.ME):
+        def wrapper(func):
+            @bot.on_message(filters.command(command) & filter)
+            async def wrapped_func(client, message):
+                await func(client, message)
+
+            return wrapped_func
+
+        return wrapper
+
+    def INLINE(command):
+        def wrapper(func):
+            @bot.on_inline_query(filters.regex(command))
+            async def wrapped_func(client, message):
+                await func(client, message)
+
+            return wrapped_func
+
+        return wrapper
+
+    def CALLBACK(command):
+        def wrapper(func):
+            @bot.on_callback_query(filters.regex(command))
+            async def wrapped_func(client, message):
+                await func(client, message)
+
+            return wrapped_func
+
+        return wrapper
