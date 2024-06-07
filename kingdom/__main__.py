0742eac8f14fc7fbaedf60836397ed6fd10bedf4
Xcilik <149920327+Xcilik@users.noreply.github.com>
2024-06-08 01:08:58 +0700

    Update __main__.py

--- kingdom/__main__.py
+++ kingdom/__main__.py
@@ -1,1 +1,18 @@
+import asyncio
+import sys
+from os import execl
+from pyrogram import idle
 
+from kingdom import *
+
+LOOP = asyncio.get_event_loop()
+
+async def main():
+    await bot.start()
+    console.info("Bot Running")
+    await asyncio.gather(loadPlugins(), idle())
+
+
+if __name__ == "__main__":
+    LOOP.run_until_complete(main())
+    console.info("Bot Stoped")
