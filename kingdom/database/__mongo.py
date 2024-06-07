e27a455553d2fcd99e4be578322f762406fef820
Xcilik <149920327+Xcilik@users.noreply.github.com>
2024-06-08 03:19:49 +0700

    Update __mongo.py

--- kingdom/database/__mongo.py
+++ kingdom/database/__mongo.py
@@ -14,37 +14,6 @@ character_statsdb = db.character_statsdb
 
 
 
-
-
-
-
-
-#function async
-
-
-#broadcast user database
-async def is_served_user(user_id: int) -> bool:
-    user = await userstartdb.find_one({"user_id": user_id})
-    if not user:
-        return False
-    return True
-
-
-async def get_served_users() -> list:
-    users_list = []
-    async for user in userstartdb.find({"user_id": {"$gt": 0}}):
-        users_list.append(user)
-    return users_list
-
-
-async def add_served_user(user_id: int):
-    is_served = await is_served_user(user_id)
-    if is_served:
-        return
-    return await userstartdb.insert_one({"user_id": user_id})
-
-
-
 #character profile database
 async def get_character_profile(user_id):
     try:
