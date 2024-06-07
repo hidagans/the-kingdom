1308172c126dcc565bc2e020716f1ff80dcdbc80
Xcilik <149920327+Xcilik@users.noreply.github.com>
2024-06-08 01:38:18 +0700

    Create start.py

---
+++ kingdom/modules/start.py
@@ -0,0 +1,25 @@
+import asyncio
+
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from kingdom.database import add_served_user
+from kingdom.core import KINGDOM, FILTERS
+
+
+@KINGDOM.CMD("start", FILTERS.PRIVATE)
+@subcribe
+async def start(client, message):
+    user_id = message.from_user.id
+    await add_served_user(user_id)
+    #character_profile = await get_character_profile(user_id)
+    home_button = [
+        [
+             InlineKeyboardButton("MY PROFILE", callback_data="my_profile"),
+             InlineKeyboardButton("KONTEN", callback_data="cb_konten")
+        ],
+        [
+            InlineKeyboardButton("BLACK MARKET", callback_data="black_market"),
+        ]
+    ]
+    await message.reply("Selamat datang kembali! Ini profil Anda:", reply_markup=InlineKeyboardMarkup(home_button))
+  
+  
