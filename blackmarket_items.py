9a3df10879736b594772435d88d87341ae75f692
Xcilik <nanarananaru1@gmail.com>
2024-06-21 22:31:13 +0700

    DB UPDATE

--- blackmarket_items.py
+++ blackmarket_items.py
@@ -1,5 +1,6 @@
 import pymongo
 import random
+from kingdom.database.__mongo import weapons, headarmors, bodyarmors, footarmors
 from datetime import datetime, timedelta
 from config import MONGO_URL
 
@@ -8,29 +9,36 @@ client = pymongo.MongoClient(MONGO_URL)
 db = client["kingdom"]
 collection = db["blackmarket_items"]
 
-# Daftar jenis item dan tier yang ingin dimasukkan
-item_types = ["weapon", "headarmor", "bodyarmor", "footarmor"]
+# Daftar jenis item yang ingin dimasukkan
+item_collections = {
+    "weapon": weapons,
+    "bodyarmor": bodyarmors,
+    "headarmor": headarmors,
+    "footarmor": footarmors
+}
+
 tiers = range(2, 9)  # Tier dari 2 hingga 8
 
 # Fungsi untuk menghasilkan harga berdasarkan tier
 def generate_price(tier):
     return random.randint(tier * 500, tier * 1000)
 
-# Memasukkan item ke dalam koleksi
-for item_type in item_types:
-    for tier in tiers:
-        item_name = f"{item_type.capitalize()} Tier {tier}"
-        price = generate_price(tier)
-        update_time = datetime.utcnow() - timedelta(days=(tier - 1))  # Update harga semakin lama seiring naiknya tier
+# Memasukkan item ke dalam koleksi blackmarket_items
+for item_type, item_collection in item_collections.items():
+    for item in item_collection.find():
+        for tier in tiers:
+            item_name = f"{item_type.capitalize()} {item['name']} Tier {tier}"
+            price = generate_price(tier)
+            update_time = datetime.utcnow() - timedelta(days=(tier - 1))  # Update harga semakin lama seiring naiknya tier
 
-        item = {
-            "name": item_name,
-            "type": item_type,
-            "tier": tier,
-            "price": price,
-            "update_time": update_time
-        }
+            new_item = {
+                "name": item_name,
+                "type": item_type,
+                "tier": tier,
+                "price": price,
+                "update_time": update_time
+            }
 
-        collection.insert_one(item)
+            collection.insert_one(new_item)
 
 print("Data Black Market telah berhasil diisi.")
