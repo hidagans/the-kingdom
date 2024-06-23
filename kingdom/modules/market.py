615ddc704767d85e82ec2afa4264443aaa2a21b5
Xcilik <nanarananaru1@gmail.com>
2024-06-23 10:50:37 +0700

    market

--- kingdom/modules/market.py
+++ kingdom/modules/market.py
@@ -8,4 +8,5 @@ async def buy_item_callback(client, callback_query: CallbackQuery):
     user_id = callback_query.from_user.id
     result = await buy_item(user_id, ObjectId(item_id))
     await callback_query.message.delete()
+    await bot.send_message(user_id, f"Kamu berhasil membeli {item_id} di market silahkan cek di inventory kamu")
     await callback_query.answer(result, show_alert=True)
