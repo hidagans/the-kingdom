be20f24156de47f7c6000272ab81e17e1bd9d85e
hidagans <58388773+hidagans@users.noreply.github.com>
2024-12-10 10:21:41 +0700

    Update clean.py

--- kingdom/modules/clean.py
+++ kingdom/modules/clean.py
@@ -8,14 +8,7 @@ async def delete_service_message(message):
         await message.delete()
         print(f"Pesan {message.id} berhasil dihapus.")
 
-# Contoh data pesan
-message = {
-    "_": "Message",
-    "id": 11782,
-    "service": "MessageServiceType.NEW_CHAT_MEMBERS"
-}
-
 @KING.CMD("clean")
 async def clean_service(client, message):
-    print("proses gan")
-    await delete_service_message(type("Message", (object,), message))
+    print("Proses gan...")
+    await delete_service_message(message)
