cfae43be46728c873e1c946ebecf3ba144c46c68
hidagans <58388773+hidagans@users.noreply.github.com>
2024-12-10 10:24:44 +0700

    Update clean.py

--- kingdom/modules/clean.py
+++ kingdom/modules/clean.py
@@ -1,14 +1,20 @@
 from kingdom import *
 from kingdom.core import *
-from pyrogram import *
-from pyrogram.types import *
+from pyrogram import Client, filters
+from pyrogram.types import Message
 
-async def delete_service_message(message):
+# Fungsi untuk menghapus pesan layanan
+async def delete_service_message(message: Message):
     if message.service == "MessageServiceType.NEW_CHAT_MEMBERS":
         await message.delete()
         print(f"Pesan {message.id} berhasil dihapus.")
 
+# Perintah untuk membersihkan pesan sebelumnya
 @KING.CMD("clean")
-async def clean_service(client, message):
-    print("Proses gan...")
-    await delete_service_message(message)
+async def clean_service(client: Client, message: Message):
+    print("Memulai pembersihan pesan layanan...")
+    
+    async for msg in client.get_chat_history(message.chat.id, limit=100):
+        await delete_service_message(msg)
+    
+    print("Pembersihan selesai.")
